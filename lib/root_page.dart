664bdad30eade23ffffe95fd7b5384e5bc30ecec
Juyeong Lee <2jy22@naver.com>
2019-12-21 00:40:49 +0900

    Firebase와 Google 로그인 기능 구현

--- lib/root_page.dart
+++ lib/root_page.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/login_page.dart';
 import 'package:instagram_clone/tab_page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class RootPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return LoginPage();
+    return StreamBuilder<FirebaseUser>(
+      stream: FirebaseAuth.instance.onAuthStateChanged,
+      builder: (BuildContext context, AsyncSnapshot snapshot) { // snapshot은 firebase user 정보가 들어온다
+        if (snapshot.hasData) { // 로그인 성공
+          return TabPage(snapshot.data);
+        } else {
+          return LoginPage();
+        }
+      },
+    );
   }
 }
